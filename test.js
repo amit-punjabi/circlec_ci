88dc7b074722164e9eb03bf6a315e56a92672466
Amit Punjabi <amitpunjabi@Amits-MacBook-Pro.local>
2018-09-27 20:51:47 +0530

    First commit

---
+++ test.js
@@ -0,0 +1,25 @@
+var webdriver = require('selenium-webdriver');
+
+// Input capabilities
+var capabilities = {
+ 'browserName' : 'iPhone',
+ 'device' : 'iPhone 7',
+ 'realMobile' : 'true',
+ 'os_version' : '10.3',
+ 'browserstack.user' : process.env.BROWSERSTACK_USERNAME,
+ 'browserstack.key' : process.env.BROWSERSTACK_ACCESS_KEY
+}
+
+var driver = new webdriver.Builder().
+  usingServer('http://hub-cloud.browserstack.com/wd/hub').
+  withCapabilities(capabilities).
+  build();
+
+driver.get('http://www.google.com').then(function(){
+  driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function(){
+    driver.getTitle().then(function(title) {
+      console.log(title);
+      driver.quit();
+    });
+  });
+});
